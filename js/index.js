9759bc1edd6edb74addd13f67d7b685ba1401252
Oksanchik777 <57568736+Oksanchik777@users.noreply.github.com>
2019-11-21 00:53:48 +0200

    Update and rename js/index.js to  JS/main.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-
-    var input=document.querySelector('input');
-    var val = input.value
-    var button=document.querySelector('button')
-
-
-    //'http://www.omdbapi.com/?apikey=f78df073&t=Batman';
-    
-    //
