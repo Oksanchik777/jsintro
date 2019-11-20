7f0bb8f0312a889ba5a177d8805f60771affe9d9
Oksanchik777 <57568736+Oksanchik777@users.noreply.github.com>
2019-11-21 00:40:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+var search = document.querySelector('.search');
+var go = document.querySelector('.go');
+
+go.addEventListener('click', myFunction);
+
+function myFunction(){
+    var  result = search.value;
+    var url = 'http://www.omdbapi.com/?apikey=f78df073&t=Batman';
+    var req = new Request(url);
+    fetch(req)
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data) => {
+        document.querySelector('h1').innerHTML = `${data.Title} ${data.Country} <br> Released: ${data.Released}`
+        var poster = document.querySelector('.poster');
+        poster.innerHTML=`<img src="${data.Poster}" alt="">`;
+    })
+}
 
